1b65d19f9e4d13c17b98970fe988124049859030
Leonard Milevsky <leokelman06@gmail.com>
2022-03-29 16:06:26 +0300

    master

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from secrets import token_urlsafe
 from aiohttp.client import *
 from aiohttp import payload, web
 from aiohttp.client import request
@@ -21,7 +22,7 @@ def handler_function(request):
     session = request['session']
     version = request['version']
     request = request['request']
-    list_of_request = request['nlu']['tokens']
+    tokens = request['nlu']['tokens']
 
 ### Start
     if session['new'] or state == 0:
@@ -42,175 +43,175 @@ def handler_function(request):
     ### Start    
     elif state == 10:
 
-        if 'код' in list_of_request or 'кот' in list_of_request:
+        if 'код' in tokens or 'кот' in tokens:
             message = ("Код переработки — это треугольник с цифрой внутри. Цифры обозначают материал, из которого сделан предмет")
             tts = "Код переработки — это треугольник с цифрой внутри. Цифры обозначают материал, из которого сделан предмет"
 
-        elif 'второе' in list_of_request or 'идентификатор' in list_of_request:
+        elif 'второе' in tokens or 'идентификатор' in tokens:
             message = ('Идентификатор переработки — это буквы на русском или латинице, расположенные под треугольником с цифрой внутри')
             tts = 'Идентификатор переработки — это буквы на русском или латинице, расположенные под треугольником с цифрой внутри'
 
-        elif 'спасибо' in list_of_request or 'стоп' in list_of_request or 'выход' in list_of_request or 'не' in list_of_request or 'заверши' in list_of_request or 'пока' in list_of_request:
+        elif 'спасибо' in tokens or 'стоп' in tokens or 'выход' in tokens or 'не' in tokens or 'заверши' in tokens or 'пока' in tokens:
             state = 100
 
 ### Main part
 
 # Paper
 
-        elif ('двадцать' in list_of_request and 'один' in list_of_request) or '21' in list_of_request:
+        elif ('двадцать' in tokens and 'один' in tokens) or '21' in tokens:
             message = mat[10] + '\n' + "Использование: " + using[10] + '\n'+ rec[10]
             tts = message
 
-        elif ('двадцать' in list_of_request and 'два' in list_of_request) or '22' in list_of_request:
+        elif ('двадцать' in tokens and 'два' in tokens) or '22' in tokens:
             message = mat[11] + '\n' + "Использование: " + using[11] + '\n'+ rec[11]
             tts = message
 
-        elif ('двадцать' in list_of_request and 'три' in list_of_request) or '23' in list_of_request:
+        elif ('двадцать' in tokens and 'три' in tokens) or '23' in tokens:
             message = mat[12] + '\n' + "Использование: " + using[12] + '\n'+ rec[12]
             tts = message
         
-        elif 'двадцать' in list_of_request or '20' in list_of_request:
+        elif 'двадцать' in tokens or '20' in tokens:
             message = mat[9] + '\n' + "Использование: " + using[9] + '\n'+ rec[9]
             tts = message
         
-        elif 'пэп' in list_of_request or 'пап' in list_of_request or 'рар' in list_of_request or 'pap' in list_of_request:
+        elif 'пэп' in tokens or 'пап' in tokens or 'рар' in tokens or 'pap' in tokens:
             message = "Целлюлозная продукция (гофрированный картон, картон, бумага)." + '\n\n' + "Использование: Коробки, открытки, журналы, газеты, офисная бумага." + '\n\n' + rec[9]
             tts = message
 
 # Metals
 
-        elif ('сорок' in list_of_request and 'один' in list_of_request) or '41' in list_of_request or 'ал' in list_of_request or 'al' in list_of_request or 'алу' in list_of_request or 'alu' in list_of_request:
+        elif ('сорок' in tokens and 'один' in tokens) or '41' in tokens or 'ал' in tokens or 'al' in tokens or 'алу' in tokens or 'alu' in tokens:
             message = mat[14] + '\n' + "Использование: " + using[14] + '\n'+ rec[14]
             tts = message
 
-        elif 'сорок' in list_of_request or 'фу' in list_of_request or '40' in list_of_request or 'fu' in list_of_request:
+        elif 'сорок' in tokens or 'фу' in tokens or '40' in tokens or 'fu' in tokens:
             message = mat[13] + '\n' + "Использование: " + using[13] + '\n'+ rec[13]
             tts = message
         
 # Organic materials of natural origin
 
-        elif ('пятьдесят' in list_of_request and 'один' in list_of_request) or '51' in list_of_request:
+        elif ('пятьдесят' in tokens and 'один' in tokens) or '51' in tokens:
             message = mat[16] + '\n' + "Использование: " + using[16] + '\n'+ rec[16]
             tts = message
 
-        elif 'пятьдесят' in list_of_request or '50' in list_of_request or 'for' in list_of_request or 'фор' in list_of_request:
+        elif 'пятьдесят' in tokens or '50' in tokens or 'for' in tokens or 'фор' in tokens:
             message = mat[15] + '\n' + "Использование: " + using[15] + '\n'+ rec[15]
             tts = message
 
-        elif ('шестьдесят' in list_of_request and 'один' in list_of_request) or '61' in list_of_request:
+        elif ('шестьдесят' in tokens and 'один' in tokens) or '61' in tokens:
             message = mat[18] + '\n' + "Использование: " + using[18] + '\n'+ rec[18]
             tts = message
         
-        elif 'шестьдесят' in list_of_request or '60' in list_of_request or 'tex' in list_of_request or 'текс' in list_of_request:
+        elif 'шестьдесят' in tokens or '60' in tokens or 'tex' in tokens or 'текс' in tokens:
             message = mat[17] + '\n' + "Использование: " + using[17] + '\n'+ rec[17]
             tts = message
 
 
 # Glass
         
-        elif ('семьдесят' in list_of_request and 'один' in list_of_request) or '71' in list_of_request:
+        elif ('семьдесят' in tokens and 'один' in tokens) or '71' in tokens:
             message = mat[20] + '\n' + "Использование: " + using[20] + '\n'+ rec[20]
             tts = message
 
-        elif ('семьдесят' in list_of_request and 'два' in list_of_request) or '72' in list_of_request:
+        elif ('семьдесят' in tokens and 'два' in tokens) or '72' in tokens:
             message = mat[21] + '\n' + "Использование: " + using[21] + '\n'+ rec[21]
             tts = message
         
-        elif ('семьдесят' in list_of_request and 'три' in list_of_request) or '73' in list_of_request:
+        elif ('семьдесят' in tokens and 'три' in tokens) or '73' in tokens:
             message = mat[22] + '\n' + "Использование: " + using[22] + '\n'+ rec[22]
             tts = message
 
-        elif ('семьдесят' in list_of_request and 'четыре' in list_of_request) or '74' in list_of_request:
+        elif ('семьдесят' in tokens and 'четыре' in tokens) or '74' in tokens:
             message = mat[23] + '\n' + "Использование: " + using[23] + '\n'+ rec[23]
             tts = message
         
-        elif ('семьдесят' in list_of_request and 'пять' in list_of_request) or '75' in list_of_request:
+        elif ('семьдесят' in tokens and 'пять' in tokens) or '75' in tokens:
             message = mat[24] + '\n' + "Использование: " + using[24] + '\n'+ rec[24]
             tts = message
 
-        elif ('семьдесят' in list_of_request and 'шесть' in list_of_request) or '76' in list_of_request:
+        elif ('семьдесят' in tokens and 'шесть' in tokens) or '76' in tokens:
             message = mat[25] + '\n' + "Использование: " + using[25] + '\n'+ rec[25]
             tts = message
 
-        elif ('семьдесят' in list_of_request and 'семь' in list_of_request) or '77' in list_of_request:
+        elif ('семьдесят' in tokens and 'семь' in tokens) or '77' in tokens:
             message = mat[26] + '\n' + "Использование: " + using[26] + '\n'+ rec[26]
             tts = message
         
-        elif ('семьдесят' in list_of_request and 'восемь' in list_of_request) or '78' in list_of_request:
+        elif ('семьдесят' in tokens and 'восемь' in tokens) or '78' in tokens:
             message = mat[27] + '\n' + "Использование: " + using[27] + '\n'+ rec[27]
             tts = message
 
-        elif ('семьдесят' in list_of_request and 'девять' in list_of_request) or '79' in list_of_request:
+        elif ('семьдесят' in tokens and 'девять' in tokens) or '79' in tokens:
             message = mat[28] + '\n' + "Использование: " + using[28] + '\n'+ rec[28]
             tts = message
 
-        elif 'семьдесят' in list_of_request or '70' in list_of_request:
+        elif 'семьдесят' in tokens or '70' in tokens:
             message = mat[19] + '\n' + "Использование: " + using[19] + '\n'+ rec[19]
             tts = message
         
 # Composite materials
-        elif ('восемьдесят' in list_of_request and 'один' in list_of_request) or '81' in list_of_request:
+        elif ('восемьдесят' in tokens and 'один' in tokens) or '81' in tokens:
             message = mat[29] + '\n' + "Использование: " + using[29] + '\n'+ rec[29]
             tts = message
 
-        elif ('восемьдесят' in list_of_request and 'два' in list_of_request) or '82' in list_of_request:
+        elif ('восемьдесят' in tokens and 'два' in tokens) or '82' in tokens:
             message = mat[30] + '\n' + "Использование: " + using[30] + '\n'+ rec[30]
             tts = message
         
-        elif ('восемьдесят' in list_of_request and 'четыре' in list_of_request) or '84' in list_of_request:
+        elif ('восемьдесят' in tokens and 'четыре' in tokens) or '84' in tokens:
             message = mat[31] + '\n' + "Использование: " + using[31] + '\n'+ rec[31]
             tts = message
        
-        elif ('девяносто' in list_of_request and 'два' in list_of_request) or '92' in list_of_request:
+        elif ('девяносто' in tokens and 'два' in tokens) or '92' in tokens:
             message = mat[33] + '\n' + "Использование: " + using[33] + '\n'+ rec[33]
             tts = message
 
-        elif ('девяносто' in list_of_request and 'восемь' in list_of_request) or '98' in list_of_request:
+        elif ('девяносто' in tokens and 'восемь' in tokens) or '98' in tokens:
             message = mat[34] + '\n' + "Использование: " + using[34] + '\n'+ rec[34]
             tts = message
         
-        elif 'девяносто' in list_of_request or '90' in list_of_request:
+        elif 'девяносто' in tokens or '90' in tokens:
             message = mat[32] + '\n' + "Использование: " + using[32] + '\n'+ rec[32]
             tts = message
             
-        elif 'эс лдпе' in list_of_request or 'эс алу' in list_of_request or 'эс пап' in list_of_request or 'эс пэпэ' in list_of_request or 'c/pap' in list_of_request or 'с/рар' in list_of_request or 'с/рр' in list_of_request or 'c/pp' in list_of_request or 'c/ldpe' in list_of_request or 'c/alu' in list_of_request:
+        elif 'эс лдпе' in tokens or 'эс алу' in tokens or 'эс пап' in tokens or 'эс пэпэ' in tokens or 'c/pap' in tokens or 'с/рар' in tokens or 'с/рр' in tokens or 'c/pp' in tokens or 'c/ldpe' in tokens or 'c/alu' in tokens:
             message = "Композиционный материал, выполненный из смеси различных материалов" + '\n\n' + "Использование: кофе, соусы, сгущенка, лапша быстрого приготовления, зубная паста, косметический крем" + '\n\n'+ rec[34]
             tts = message
 
 # Plastic
-        elif 'один' in list_of_request or '01' in list_of_request or '1' in list_of_request or 'пэт' in list_of_request or 'pet' in list_of_request or 'pete' in list_of_request:
+        elif 'один' in tokens or '01' in tokens or '1' in tokens or 'пэт' in tokens or 'pet' in tokens or 'pete' in tokens:
             message = mat[0] + '\n'+ "Использование: " + using[0] + '\n' + rec[0]
             tts = message
 
-        elif 'два' in list_of_request or '02' in list_of_request or '2' in list_of_request or 'пэ' in list_of_request or 'пнд' in list_of_request or 'пэвп' in list_of_request or 'ашдипии' in list_of_request or 'хдпе' in list_of_request or 'hdpe' in list_of_request or 'pehd' in list_of_request:
+        elif 'два' in tokens or '02' in tokens or '2' in tokens or 'пэ' in tokens or 'пнд' in tokens or 'пэвп' in tokens or 'ашдипии' in tokens or 'хдпе' in tokens or 'hdpe' in tokens or 'pehd' in tokens:
             message = mat[1] + '\n' + "Использование: " + using[1] + '\n'+ rec[1]
             tts = message
 
-        elif 'три' in list_of_request or '03' in list_of_request or '3' in list_of_request or 'пвх' in list_of_request or 'пвс' in list_of_request or 'пвк' in list_of_request or 'pvc' in list_of_request:
+        elif 'три' in tokens or '03' in tokens or '3' in tokens or 'пвх' in tokens or 'пвс' in tokens or 'пвк' in tokens or 'pvc' in tokens:
             message = mat[2] + '\n' + "Использование: " + using[2] + '\n'+ rec[2]
             tts = message
 
-        elif 'четыре' in list_of_request or '04' in list_of_request or '4' in list_of_request or 'пвд' in list_of_request or 'пэнп' in list_of_request or 'лдпе' in list_of_request or 'ldpe' in list_of_request or 'peld' in list_of_request:
+        elif 'четыре' in tokens or '04' in tokens or '4' in tokens or 'пвд' in tokens or 'пэнп' in tokens or 'лдпе' in tokens or 'ldpe' in tokens or 'peld' in tokens:
             message = mat[3] + '\n' + "Использование: " + using[3] + '\n'+ rec[3]
             tts = message
 
-        elif 'пять' in list_of_request or '05' in list_of_request or '5' in list_of_request or 'пп' in list_of_request or 'pp' in list_of_request or 'рр' in list_of_request:
+        elif 'пять' in tokens or '05' in tokens or '5' in tokens or 'пп' in tokens or 'pp' in tokens or 'рр' in tokens:
             message = mat[4] + '\n' + "Использование: " + using[4] + '\n'+ rec[4]
             tts = message
 
-        elif 'шесть' in list_of_request or '06' in list_of_request or '6' in list_of_request or 'пс' in list_of_request or 'ps' in list_of_request:
+        elif 'шесть' in tokens or '06' in tokens or '6' in tokens or 'пс' in tokens or 'ps' in tokens:
             message = mat[5] + '\n' + "Использование: " + using[5] + '\n'+ rec[5]
             tts = message
 
-        elif 'семь' in list_of_request or '07' in list_of_request or '7' in list_of_request or 'о' in list_of_request or 'o' in list_of_request or 'other' in list_of_request or 'другое' in list_of_request:
+        elif 'семь' in tokens or '07' in tokens or '7' in tokens or 'о' in tokens or 'o' in tokens or 'other' in tokens or 'другое' in tokens:
             message = mat[6] + '\n' + "Использование: " + using[6] + '\n'+ rec[6]
             tts = message
 
-        elif 'абс' in list_of_request or 'abs' in list_of_request:
+        elif 'абс' in tokens or 'abs' in tokens:
             message = mat[7] + '\n' + "Использование: " + using[7] + '\n'+ rec[7]
             tts = message
 
-        elif 'пк' in list_of_request or 'рс' in list_of_request or 'pc' in list_of_request:
+        elif 'пк' in tokens or 'рс' in tokens or 'pc' in tokens:
             message = mat[8] + '\n' + "Использование: " + using[8] + '\n'+ rec[8]
             tts = message
     
