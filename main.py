95d8f2b135e34efbff21a53b7bae037eda99b03c
Leonard Milevsky <leokelman06@gmail.com>
2022-03-29 14:46:03 +0300

    master

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def handler_function(request):
     session = request['session']
     version = request['version']
     request = request['request']
-    list_of_request = request['request']['command']
+    list_of_request = request['command']
 
 ### Start
     if session['new'] or state == 0:
